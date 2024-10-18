2dc90dc7f702c2aac8799cf20ee021b9ea50598e
Natan Sant'anna <santannaferreiran@gmail.com>
2024-10-18 11:01:12 -0300

    Add files via upload
    Terceira e quarta aula de Javascript

---
+++ JScoding4/facil.js
@@ -0,0 +1,132 @@
+/*//ex1
+function maiorDeIdade(anoNascimento1) {
+    const anoAtual1 = new Date().getFullYear();
+    const idade = anoAtual1 - anoNascimento1;
+    if (idade >= 18) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+//ex2
+
+  let nota1 = 7;
+  let nota2 = 8;
+  let nota3 = 6;
+
+  let notafinal = (nota1 + nota2 + nota3) / 3;
+
+  if (notafinal >= 7){
+    console.log (aprovado)
+  }
+//ex3
+  let idade = 15 
+  if (idade >= 16) {
+    console.log (voce ode votar);
+  } else{
+    console.log(reporvasdo)
+  }
+//ex4
+let n1 = 50
+let n2 = 60
+  if (n1 > n2)
+
+
+//ex5
+let segunda = 8;
+let terça = 8;
+let quarta = 8; 
+let quinta = 8;
+let sexta = 8;
+
+if ((segunda + terça + quarta + quinta + sexta) === 40) {
+    console.log(parabens );
+}
+//ex6
+
+let tip = 55;
+if ( tip >= 10 && tip <= 50) {
+    console.log9 (`seu numero é ${tip} e não numero primo`)
+} else {
+    console.lof ( `seu numero é ${tip} e não é numero primo`)
+}
+
+let vogal = [ a, e, i, o, u];
+let word = `1`;
+const filtro = vogal.includes(word);
+console.log(filtro);
+
+let mes = `janeiro`
+let ferias = [janeiro, fevereiro ,dezembro]
+if (ferias.includes(mes)) {
+    console.log(férias);
+
+} else {
+    console.log 
+}
+
+//ex7
+let pontuacao = 4;
+if pontuacao (>= 9 && pontuacao <=10){
+    console.log(`excelente`)
+}
+//ex8
+ let numero1 =480;
+ let numero2 =900;
+
+ console.log(numero1 > numero2 ? numero2 : numero1)
+ 
+//ex9
+
+let temperatura = 22;
+if  temperatura < 15 
+
+console.log
+
+//ex10
+let nprimo = 9;
+if(nprimo % nprimo === 0 && nprimo %2 ! == 0 && nprimo % 3 !== 0 && nprimo <==100 ) {
+    console.log(fala)
+} else if ( nprimo % nprimo === 0 && nprimo % 2 === 0 barra barra nprimo % 3 === 0 barra barra  nprimo >=100 ){
+    console.log (alguma coisa)
+} console.log(`${nprimo} é um numero primo que não está entre os numeros até 100`)
+
+//ex11
+
+let semaforo = `verde`;
+if (semaforo === verde){
+    console.log(seguir)
+}
+    else if 
+
+//ex 12
+
+let number = 7;
+let soma = 0;
+for (let 1 = 1; i <=n /2; i ++) {
+    if(Number % 1 ===0){
+        soma +=i;
+    }
+}
+
+if (soma ===number) {
+    console.log(é perfeito);
+} else {
+    console.log (não é perfeito);
+}
+
+//ex13
+
+let lado1 =
+let lado2 =
+let lado3 = 
+
+
+
+
+
+//ex
+let age = 18;
+if age (=>.0 && age <= 12) 
+{console.log(`criança`)} 
+else if ( age >= 13 && age <= )
---
+++ JScoding4/function.js
@@ -0,0 +1,6 @@
+
+function saudação(nome){
+    return "ola" + nome + "!"
+}
+const mensagem = saudação("maria do rosario")
+console.log(mensagem)
